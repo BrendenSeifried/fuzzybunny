8c8cdaa89f53f4485f46596359b4eb85dbe0a3db
BrendenSeifried <brendenseifried@gmail.com>
2022-02-21 16:48:14 -0800

    cleaning up code

--- create/create.js
+++ create/create.js
@@ -11,26 +11,15 @@ const logoutButton = document.getElementById('logout');
 form.addEventListener('submit', async e => {
     e.preventDefault();
     const formData = new FormData(form);
-    //const name = formData.get('bunny-name');
-    //const familyId = formData.get('family-id');
     const bunnie = { name: formData.get('bunny-name'), familyId: formData.get('family-id') };
     console.log(bunnie);
     await createBunny(bunnie);
-    // location.replace('../families');
-    // prevent default
-
-    // get the name and family id from the form
-
-    // use createBunny to create a bunny with this name and family id
     
     form.reset();
 });
 
 window.addEventListener('load', async () => {
-    // let's dynamically fill in the families dropdown from supabase
-    // grab the select HTML element from the DOM
-    // const select = document.querySelector('select');
-    // go get the families from supabase
+
     const select = document.querySelector('select');
     const families = await getFamilies();
     for (let family of families) {
@@ -39,13 +28,6 @@ window.addEventListener('load', async () => {
         option.textContent = family.name;
         select.append(option);
     }
-    // for each family
-
-    // create an option tag
-
-    // set the option's value and text content
-
-    // and append the option to the select
 });
 
 
